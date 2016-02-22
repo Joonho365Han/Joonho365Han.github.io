7b14b9e324463cffc6be98f3824bf557550ece3b
Joonho Han <corpuscallosum@wireless1x-155-41-124-117.bu.edu>
2016-02-22 02:45:24 -0500

    Slideshow test

--- JavaScript/home_slick.js
+++ JavaScript/home_slick.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
   $('.slideshow').slick({
     setting-name: setting-value
+	adaptiveHeight: true;
+	arrows: false;
+	autoplay: true;
+	speed: 300;
+	autoplaySpeed: 10000;
+	accessibility: false;
+	draggable: false;
   });
-});
-
-$('.slideshow').slick({
-	infinite: true;
 });
