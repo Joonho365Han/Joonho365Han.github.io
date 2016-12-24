758b2cc9d90395916d141cad168765c4e730d459
Joonho Han <joonhohan365@gmail.com>
2016-12-24 23:01:42 +0900

    Test blog post fade in animation

--- JS/auto_load.js
+++ JS/auto_load.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 
-    var postNumber = 4;
+    var postNumber = 1;
     $(window).scroll(function(){
-        if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
-
+        if ($(window).scrollTop() >= $(document).height() - $(window).height())
+        {
             var loadMore = 3;
-            while (postNumber && loadMore--){
+            while (postNumber && loadMore--)
+            {
                 $.get("../Posts/blogpost_" + postNumber-- + ".html", function(DOM){
                     $("#blogpost_container").append(DOM);
                 });
