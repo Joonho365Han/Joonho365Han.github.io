a58c4be01d8034b1ae58a03bc198b84ef4233051
Joonho Han <joonhohan365@gmail.com>
2016-12-24 21:56:40 +0900

    Move where jquery is loaded

--- JS/auto_load.js
+++ JS/auto_load.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
     alert("jquery loaded!");
-    int postNumber = 4;
+    var postNumber = 4;
 
     $(window).scroll(function(){
         if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
 
-            int loadMore = 3;
+            var loadMore = 3;
 
             if (postNumber && loadMore){
                 $("#blogpost_container").append($("blogpost_" + postNumber + ".html").contents());
