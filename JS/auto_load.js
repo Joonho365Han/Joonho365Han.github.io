8c3c03b9a5e125fc4db05de1970c8a3b2786b8af
Joonho Han <joonhohan365@gmail.com>
2016-12-24 22:44:04 +0900

    Test new auto load script

--- JS/auto_load.js
+++ JS/auto_load.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
 
     var postNumber = 4;
-
     $(window).scroll(function(){
         if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
 
-            alert("at bottom");
             var loadMore = 3;
-
-            if (postNumber && loadMore){
-                $("#blogpost_container").append($("blogpost_" + postNumber + ".html").contents());
+            while (postNumber && loadMore){
+                $.get("../Posts/blogpost_" + postNumber + ".html", function(DOM){
+                    $("#blogpost_container").append(DOM);
+                });
                 postNumber -= 1;
                 loadMore   -= 1;
             }
