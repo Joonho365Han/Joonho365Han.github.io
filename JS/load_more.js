b4af5a1a73fea4ce33f4ceb43998ee33d0938947
Joonho Han <joonhohan365@gmail.com>
2016-12-24 23:47:30 +0900

    Revert "synchronous" post. It's an error.

--- JS/load_more.js
+++ JS/load_more.js
@@ -4,14 +4,16 @@ $(document).ready(function(){
 
     //  If webpage is shorter than browser, pre-load posts.
     while ($(window).scrollTop() >= $(document).height() - $(window).height() && postNumber){
-        $("#blogpost_container").append($.get("../Posts/post_" + postNumber-- + ".html"));
+        $.get("../Posts/post_" + postNumber-- + ".html", function(DOMstring){
+            $("#blogpost_container").append(DOMstring);
+        });
     }
 
     //  Load more whenever scroll reaches bottom
     $(window).scroll(function(){
         if ($(window).scrollTop() >= $(document).height() - $(window).height())
         {
-            var loadMore = 3;
+            var loadMore = 10;
             while (postNumber && loadMore--)
             {
                 $.get("../Posts/post_" + postNumber-- + ".html", function(DOMstring){
