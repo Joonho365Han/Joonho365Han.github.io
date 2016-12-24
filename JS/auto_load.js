a54bc22dca568dbb390f9d45f77a9176f1446a4a
Joonho Han <joonhohan365@gmail.com>
2016-12-24 23:23:51 +0900

    Test pre-load page with post script

--- JS/auto_load.js
+++ JS/auto_load.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
 
     var postNumber = 7;
+
+    //  If window is taller than website, pre-load posts.
+    while ($(window).scrollTop() >= $(document).height() - $(window).height() && postNumber){
+        $.get("../Posts/post_" + postNumber-- + ".html", function(DOMstring){
+            $("#blogpost_container").append(DOMstring);
+        });
+    }
+
+    //  Load more whenever scroll reaches bottom
     $(window).scroll(function(){
         if ($(window).scrollTop() >= $(document).height() - $(window).height())
         {
             var loadMore = 6;
             while (postNumber && loadMore--)
             {
-                $.get("../Posts/blogpost_" + postNumber-- + ".html", function(DOM){
-                    $("#blogpost_container").append(DOM);
+                $.get("../Posts/post_" + postNumber-- + ".html", function(DOMstring){
+                    $("#blogpost_container").append(DOMstring);
                 });
             }
         }
