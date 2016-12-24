84fe516910c2580902d5540fd01807914bbcc15e
Joonho Han <joonhohan365@gmail.com>
2016-12-24 23:40:05 +0900

    when pre-loading posts, make the document extension synchronous

--- JS/load_more.js
+++ JS/load_more.js
@@ -4,16 +4,14 @@ $(document).ready(function(){
 
     //  If webpage is shorter than browser, pre-load posts.
     while ($(document).height() < $(window).height() && postNumber){
-        $.get("../Posts/post_" + postNumber-- + ".html", function(DOMstring){
-            $("#blogpost_container").append(DOMstring);
-        });
+        $("#blogpost_container").append($.get("../Posts/post_" + postNumber-- + ".html"));
     }
 
     //  Load more whenever scroll reaches bottom
     $(window).scroll(function(){
         if ($(window).scrollTop() >= $(document).height() - $(window).height())
         {
-            var loadMore = 6;
+            var loadMore = 3;
             while (postNumber && loadMore--)
             {
                 $.get("../Posts/post_" + postNumber-- + ".html", function(DOMstring){
