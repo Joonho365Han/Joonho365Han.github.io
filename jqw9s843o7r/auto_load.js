6488b5e037c9c6323a2f1b323a596f6e31a47e62
Joonho Han <joonhohan365@gmail.com>
2016-12-24 17:18:06 +0900

    Clean up CSS and reduce parameters

---
+++ jqw9s843o7r/auto_load.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    int postNumber = 4;
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
+
+            int loadMore = 3;
+
+            if (postNumber && loadMore){
+                $("#blogpost_container").append($("blogpost_" + postNumber + ".html").contents());
+                postNumber -= 1;
+                loadMore   -= 1;
+            }
+        }
+    });
+});
