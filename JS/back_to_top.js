2681ad7f69c43ee198b930a540fc692adb88c0f3
Joonho Han <joonhohan365@gmail.com>
2016-12-26 05:15:33 +0900

    Make button visible only when scrolled down

--- JS/back_to_top.js
+++ JS/back_to_top.js
@@ -1,5 +1,10 @@
 $(function(){
-    $('a[id="button_to_top"]').click(function(){
+    $(window).scroll(function(){
+        $('a[id="button_to_top"]').css('visibility', ($(window).scrollTop() >= $(window).height()) ? 'visible' : 'hidden');
+    });
+
+    $('a[id="button_to_top"]').click(function(e){
+        e.preventDefault();
         $('html, body').animate({ scrollTop: 0 }, 800);
     });
 });
