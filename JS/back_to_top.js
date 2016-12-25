845fec990f88b12a0cb35c733310c2ce770b4da7
Joonho Han <joonhohan365@gmail.com>
2016-12-26 08:49:43 +0900

    Test fade in button

--- JS/back_to_top.js
+++ JS/back_to_top.js
@@ -1,6 +1,13 @@
 $(function(){
     $(window).scroll(function(){
-        $('a[id="button_to_top"]').css('visibility', ($(window).scrollTop() >= $('div[id="blogpost_container"]').offset().top) ? 'visible' : 'hidden');
+        if ($(window).scrollTop() >= $('div[id="blogpost_container"]').offset().top)
+        {
+            $('a[id="button_to_top"]').css('visibility', 'visible').css('animation-name', 'half_fade_in').css('-webkit-animation-name', 'half_fade_in');
+        }
+        else
+        {
+            $('a[id="button_to_top"]').css('visibility', 'hidden').css('animation-name', 'none').css('-webkit-animation-name', 'none');
+        }
     });
 
     $('a[id="button_to_top"]').click(function(e){
