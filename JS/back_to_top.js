35835c2ee84e58341a04f7c539e149ca99314ce3
Joonho Han <joonhohan365@gmail.com>
2016-12-26 09:31:03 +0900

    Revert all button fade logic

--- JS/back_to_top.js
+++ JS/back_to_top.js
@@ -1,9 +1,6 @@
 $(function(){
     $(window).scroll(function(){
-        if ($(window).scrollTop() >= $('div[id="blogpost_container"]').offset().top)
-            $('a[id="button_to_top"]').fadeTo(250, 0.4);
-        else
-            $('a[id="button_to_top"]').fadeTo(250, 0);
+        $('a[id="button_to_top"]').css('visibility', ($(window).scrollTop() >= $('div[id="blogpost_container"]').offset().top) ? 'visible' : 'hidden');
     });
 
     $('a[id="button_to_top"]').click(function(e){
