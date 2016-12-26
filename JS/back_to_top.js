b8f4086d5cddfd86ed717a2831fc21307b7276ce
Joonho Han <joonhohan365@gmail.com>
2016-12-26 09:29:22 +0900

    Change button fade logic

--- JS/back_to_top.js
+++ JS/back_to_top.js
@@ -1,15 +1,9 @@
 $(function(){
     $(window).scroll(function(){
         if ($(window).scrollTop() >= $('div[id="blogpost_container"]').offset().top)
-        {
-            $('a[id="button_to_top"]').css('visibility', 'visible').fadeTo(250, 0.4);
-        }
+            $('a[id="button_to_top"]').fadeTo(250, 0.4);
         else
-        {
-            $('a[id="button_to_top"]').fadeTo(250, 0, function(){
-                $('a[id="button_to_top"]').css('visibility', 'hidden');
-            });
-        }
+            $('a[id="button_to_top"]').fadeTo(250, 0);
     });
 
     $('a[id="button_to_top"]').click(function(e){
