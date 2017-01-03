a716ff858e17cda5d6abf0aa566ec47952471f8c
Joonho Han <joonhohan365@gmail.com>
2017-01-04 02:47:37 +0900

    Edit to use postblocks instead of individual posts

--- JS/load_more.js
+++ JS/load_more.js
@@ -1,22 +1,19 @@
 $(function(){
 
-    var postCount = 3;
+    //  Must be 1 or more.
+    var postblockCount = 1;
 
     //  Pre-load some posts just in case webpage is shorter than browser.
-    for (var i=0; i<10 && postCount; i++)
-        $.get("../Posts/post_" + postCount-- + ".html", function(DOMstring){
-            $("#blogpost_container").append(DOMstring);
-        });
+    $.get("../Posts/postblock_" + postblockCount-- + ".html", function(DOMstring){
+        $("#blogpost_container").append(DOMstring);
+    });
 
     //  Load more whenever scroll reaches bottom
     $(window).scroll(function(){
         if ($(window).scrollTop() >= $(document).height() - $(window).height())
-        {
-            var loadMore = 10;
-            while (postCount && loadMore--)
-                $.get("../Posts/post_" + postCount-- + ".html", function(DOMstring){
+            if (postblockCount)
+                $.get("../Posts/postblock_" + postblockCount-- + ".html", function(DOMstring){
                     $("#blogpost_container").append(DOMstring);
                 });
-        }
     });
 });
